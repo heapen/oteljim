9785823cde1c0186afc694885e2e713101f10a99
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-09-08 13:56:38 +0300

    Button and Register added

--- app/components/navbar/modals/Modal.tsx
+++ app/components/navbar/modals/Modal.tsx
@@ -1,9 +1,48 @@
 'use client'
+import Button from '../../buttons/Button';
+import './modal.css'
+import {GrClose} from 'react-icons/Gr'
 
-const Modal = () => {
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onSumbit: () => void;
+  btnLabel: string;
+  title: string;
+  bodyElement?: React.ReactElement;
+  footerElement?: React.ReactElement;
+}
+
+const Modal:React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSumbit,
+  btnLabel,
+  title,
+  bodyElement,
+  footerElement
+}) => {
+  const closeFunc = () => {
+    onClose();
+  }
+  const sumbitFunc = () => {
+    onSumbit();
+  }
+  
   return (
-    <div>
-      
+    <div className="modal">
+      <div className='modal-box'>
+        <div className='modal-box-text'>
+          <div className='modal-box-text-title'>{title}</div>
+          <div onClick={closeFunc}>
+            <GrClose size={25}/>
+          </div>
+        </div>
+        <Button
+          onSubmit={sumbitFunc}
+          btnLabel={btnLabel} 
+        />
+      </div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
+'use client'
+import MountedClient from "./components/MountedClient"
 import Navbar from "./components/navbar/Navbar"
 import {Nunito} from 'next/font/google'
+import Modal from "./components/navbar/modals/Modal"
 
 const newFont = Nunito({
     subsets: ['latin']
@@ -9,7 +12,10 @@ const RootLayout = ({children} : {children: React.ReactNode}) => {
   return (
    <html lang="en">
         <body className={newFont.className}>
+            <MountedClient>
+            <Modal isOpen onSumbit={() => {}} onClose={() => {}} btnLabel="Register" title="Register"/>
             <Navbar/>
+            </MountedClient>
             {children}
         </body>
    </html>
